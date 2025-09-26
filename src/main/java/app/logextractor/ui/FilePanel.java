402aa9e10fb8c30408fca0db2804f1c6aae63fa7
RiSE4 <watakun0321@gmail.com>
2025-09-26 10:35:28 +0900

    DDでファイルをロードできるように修正

--- src/main/java/app/logextractor/ui/FilePanel.java
+++ src/main/java/app/logextractor/ui/FilePanel.java
@@ -1,7 +1,9 @@
 package app.logextractor.ui;
 
 import javax.swing.*;
+import java.awt.datatransfer.DataFlavor;
 import java.io.File;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class FilePanel extends JPanel {
@@ -20,6 +22,7 @@ public class FilePanel extends JPanel {
         add(Box.createHorizontalStrut(12));
         add(selectFileButton);
 
+        //ボタンでファイルを選択
         selectFileButton.addActionListener(e -> {
             JFileChooser chooser = new JFileChooser();
             int ret = chooser.showOpenDialog(this);
@@ -29,6 +32,36 @@ public class FilePanel extends JPanel {
                 onFileSelected.accept(file);
             }
         });
+
+        //DDでファイルを選択
+        fileField.setTransferHandler(new TransferHandler() {
+            public boolean canImport(TransferSupport support) {
+                return support.isDataFlavorSupported(DataFlavor.javaFileListFlavor);
+            }
+
+            public boolean importData(TransferSupport support) {
+                if (!canImport(support)) {
+                    return false;
+                }
+                try {
+                    Object data = support.getTransferable().getTransferData(DataFlavor.javaFileListFlavor);
+                    if (data instanceof List<?> list) {
+                        if (!list.isEmpty() && list.getFirst() instanceof File file) {
+                            setFile(file, onFileSelected);
+                            return true;
+                        }
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                return false;
+            }
+        });
+    }
+
+    private void setFile(File file, Consumer<File> onFileSelected) {
+        fileField.setText(file.getAbsolutePath());
+        onFileSelected.accept(file);
     }
 
     public void reset() {
